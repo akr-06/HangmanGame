101b174a63f24ec83ea15e129a69dbb0de96dffc
Amit Kumar Roy <akumarroy86@gmail.com>
2024-11-22 12:32:30 +0530

    fix: message

--- Hangman game/src/pages/startGame.jsx
+++ Hangman game/src/pages/startGame.jsx
@@ -4,7 +4,7 @@ function StartGame() {
     return (
         <>
             <div className='text-center text-2xl'>
-                <h1 >Game - Guess the word</h1><br />
+                <h1 >Game - Enter the word you want someone to guess.</h1><br />
                 <h2><b>Note:</b> If you don`t enter any word a random <b>fruit</b> name will be picked.</h2><br />
                 <TextInputFormContainer></TextInputFormContainer>
             </div>
